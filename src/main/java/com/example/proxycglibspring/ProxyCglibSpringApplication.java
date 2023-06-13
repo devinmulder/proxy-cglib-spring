0acffc469e5ea5218f6b42bd59a0098cfc23fc40
amd <amd@devin.com>
2023-06-13 10:10:56 +0900

    Initial Commit.

---
+++ src/main/java/com/example/proxycglibspring/ProxyCglibSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.proxycglibspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyCglibSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyCglibSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proxycglibspring/ProxyCglibSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proxycglibspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyCglibSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
